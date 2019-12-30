e3241993e01cd2e2bc00f6202fcb1dd14dd7cdbe
Zubin Pratap <zubin.pratap@gmail.com>
2019-12-31 01:11:10 +1100

    fix author concat function

--- src/Components/Bookdata.tsx
+++ src/Components/Bookdata.tsx
@@ -19,13 +19,14 @@ export default BookdataView;
 
 function renderAuthors(authors: Array<{ url: string; name: string }>) {
   if (authors.length === 1) return authors[0].name;
+  if (authors.length === 2) return `${authors[0].name} and ${authors[1].name}`;
 
   let str = "";
   authors.forEach((auth, ind) => {
-    if ((ind = authors.length - 1)) {
-      str += "and";
+    if (ind === authors.length - 1) {
+      str += " and ";
     }
-    str += `,${auth.name}`;
+    str += `${auth.name}`;
   });
 
   return str;
