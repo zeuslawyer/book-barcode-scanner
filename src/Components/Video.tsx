09bf8946f1e5299d03f04cc03eaee41b1e78358f
Zubin Pratap <zubin.pratap@gmail.com>
2020-02-05 13:07:38 +1100

    interim.  refactor to use two separate use effects. init scanner separately

--- src/Components/Video.tsx
+++ src/Components/Video.tsx
@@ -1,9 +1,9 @@
 // @ts-nocheck
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
-import { BrowserBarcodeReader } from '@zxing/library'; // reference:  https://zxing-js.github.io/library/examples/barcode-camera/
-import { BookListUi } from './BookListUi';
+import { BrowserBarcodeReader } from "@zxing/library"; // reference:  https://zxing-js.github.io/library/examples/barcode-camera/
+import { BookListUi } from "./BookListUi";
 
 // REFERENCE:  examples: https://zxing-js.github.io/library/
 
@@ -22,17 +22,15 @@ interface Props {}
 
 export const VideoRoot: React.FC<Props> = () => {
   const [scannedCode, setScannedCode] = React.useState<string>(null);
-  const [books, addToBooks] = React.useState<Books>({});
+  const [scannerReady, setScannerReady] = React.useState<boolean>(false);
+  const [books, updateBooks] = React.useState<Books>({});
   let [selectedCameraId, setSelectedCameraId] = React.useState(null);
   let [availableCameras, setAvailableCameras] = React.useState([]);
 
-  // find the cameras and set them, run once only on mount
-  React.useEffect(() => {
+  let codeReader = new BrowserBarcodeReader(2000);
+
+  async function scannerInit() {
     // on component mount and re-renders:
-    console.log('use memo to load scanner and video inputs fired');
-
-    let codeReader = new BrowserBarcodeReader(2000);
-
     codeReader.getVideoInputDevices().then(videoInputDevices => {
       // set up available cameras - desktop vs mobile, for renderDropDown()
       setAvailableCameras(videoInputDevices);
@@ -40,35 +38,28 @@ export const VideoRoot: React.FC<Props> = () => {
       // if no selected camera default to first one
       !selectedCameraId && setSelectedCameraId(videoInputDevices[0].deviceId);
     });
-    // start continuous reading from camera
-    if (!scannedCode) startScanning(codeReader);
-
-    // cleanup on unmount
-    return () => {
-      codeReader = undefined;
-    };
-  }, [selectedCameraId, scannedCode]);
-
+  }
   // scanning helper function
   function startScanning(codeReader: BrowserBarcodeReader) {
-    console.log('startScanning() fired');
+    console.log("startScanning() fired");
     codeReader
-      .decodeOnceFromVideoDevice(selectedCameraId, 'video-element')
+      .decodeOnceFromVideoDevice(selectedCameraId, "video-element")
       .then(res => {
         // res.text is the scanned ISBN code. if its already in state no need to update state
-        if (res.text === scannedCode) return;
         setScannedCode(res.text);
-
         // if this book has not been added to list, hit the api
         if (books[res.text] === undefined) {
           fetchBookData(res);
         } else {
           // TODO:  alert that already scanned
+          alert("Book already in your list");
         }
         // after decoding, update UI to show scan has been done, and reset state after timeout to trigger codeReader to refresh and restart scanning
+        console.log("TESTING", res.text, scannedCode);
+
         window.setTimeout(() => {
           setScannedCode(null);
-        }, 3000);
+        }, 2500);
       });
   }
 
@@ -81,6 +72,7 @@ export const VideoRoot: React.FC<Props> = () => {
         let { data } = response;
         if (Object.keys(data).length === 0) {
           // TODO:  create error messages
+          alert("No data available on book.");
           return;
         }
         // else
@@ -93,23 +85,47 @@ export const VideoRoot: React.FC<Props> = () => {
 
         // update list only if not already there
         let allBooks = { ...books, [res.text]: fetchedBook };
-        console.log('updating book list with', allBooks);
+        console.log("updating book list with", allBooks);
 
-        addToBooks(allBooks);
+        updateBooks(allBooks);
         return;
       })
       .catch(e => console.log(`Error: ${e}`));
   };
+
+  // initial scanner init, once on mount
+  React.useEffect(() => {
+    console.log("1st effect: scanner init fired");
+    scannerInit();
+    setScannerReady(true);
+
+    return () => {
+      codeReader = undefined;
+    };
+  }, []);
+
+  // once scanner initialised, start scanning
+  React.useEffect(() => {
+    console.log("2nd effect: starting to scan");
+
+    // start continuous reading from camera
+    if (scannedCode === null && scannerReady) startScanning(codeReader);
+
+    // cleanup on unmount
+    return () => {
+      codeReader = undefined;
+    };
+  }, [scannerReady, selectedCameraId, scannedCode]);
 
   const renderDropdown = () => {
     if (availableCameras.length < 2) {
       return null;
     } else {
       return (
-        <div id='sourceSelectPanel'>
-          <label htmlFor='sourceSelect'>Change video source:</label>
+        <div id="sourceSelectPanel">
+          <label htmlFor="sourceSelect">Change video source:</label>
           <select
-            id='sourceSelect'
+            id="sourceSelect"
             value={selectedCameraId}
             onChange={e => {
               setSelectedCameraId(e.target.value);
@@ -131,12 +147,12 @@ export const VideoRoot: React.FC<Props> = () => {
     <>
       <div>
         {renderDropdown()}
-        <video id='video-element' width='500' height='250'></video>
+        <video id="video-element" width="500" height="250"></video>
       </div>
       <div>
         <p>
           {scannedCode ? (
-            <span style={{ color: 'green', textTransform: 'uppercase' }}>
+            <span style={{ color: "green", textTransform: "uppercase" }}>
               Scanned!
             </span>
           ) : (
